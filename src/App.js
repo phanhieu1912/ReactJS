b4a9048eff230ce7b13811842bfbe1842faada29
Hieu <Hieu@gmail.com>
2023-06-07 23:44:14 +0700

    React Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import React,{Component} from 'react';
+import Helloclasscomponent from './Component/Helloclasscomponent';
+import Hellofuntioncomponent from './Component/Hellofuntioncomponent'
+import DemoCountClassComponents from './Component/DemoCountClassComponents';
+import DemoFuntionCountComponents from './Component/DemoFuntionCountComponents';
+import Props from './Component/Props';
+import MenuComponent from './Component/MenuComponent';
+import { DISHES } from './shared/dishes';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            {/* <header className='App-header'> */}
+            {/* <Helloclasscomponent/>
+             <Hellofuntioncomponent/>
+            <DemoCountClassComponents/>
+            <DemoFuntionCountComponents/>
+            <Props/> */}
+            </div>
+            {/* </header>
+           
+          </div> */}
+       </Navbar>
+       <MenuComponent dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/Component/Props.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+class HelloAsClass extends Component{
+    render(){
+        return <div>Hello,{this.props.who}</div>
+    }
+}
+export default HelloAsClass
